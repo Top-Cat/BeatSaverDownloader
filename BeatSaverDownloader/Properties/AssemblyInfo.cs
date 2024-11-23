4b9f6e2f609ed3379a90df9d93e355eac8286fba
Thomas Cheyney <top_cat@thomasc.co.uk>
2024-11-23 09:52:10 +0000

    Bump version

--- BeatSaverDownloader/Properties/AssemblyInfo.cs
+++ BeatSaverDownloader/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("2e037c8b-bc3e-41ad-988f-8631ad4828fb")]
 
-[assembly: AssemblyVersion("6.0.6.0")]
-[assembly: AssemblyFileVersion("6.0.6.0")]
+[assembly: AssemblyVersion("6.0.7.0")]
+[assembly: AssemblyFileVersion("6.0.7.0")]
